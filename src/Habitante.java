d1922090966a6e39bc1ed161f08ccd2822efa538
elmoyeet <100579650+elmoyeet@users.noreply.github.com>
2026-03-04 09:50:11 +0100

    Clase habitante con atributos privados, getters, metodo toString

--- src/Habitante.java
+++ src/Habitante.java
@@ -1,2 +1,43 @@
 public class Habitante {
+
+    private int id;
+    private String nombre;
+    private String especie;
+    private int nivelFelicidad;
+
+    public Habitante(int id, String nombre, String especie, int nivelFelicidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.nivelFelicidad = nivelFelicidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public int getNivelFelicidad() {
+        return nivelFelicidad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Habitantes: {");
+        sb.append("id=").append(this.id);
+        sb.append(", nombre=").append(this.nombre);
+        sb.append(", especie=").append(this.especie);
+        sb.append(", nivelFelicidad=").append(this.nivelFelicidad);
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
